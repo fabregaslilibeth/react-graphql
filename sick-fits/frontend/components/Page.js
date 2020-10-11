d972d5caa5d47b810e83c5e87dc3c95f126a6359
Lilibeth <fabregaslilibeth@gmail.com>
2020-10-11 19:58:06 +0800

    learn diff ways to add style

--- sick-fits/frontend/components/Page.js
+++ sick-fits/frontend/components/Page.js
@@ -1,13 +1,27 @@
 import React, { Component } from 'react';
 import Header from './Header';
 import Meta from "./Meta";
+import styled from "styled-components";
 
+
+const MyButton = styled.button`
+  background: red;
+  font-size: ${props => (props.huge ? '100px' : '20px')};
+  .fire {
+    font-size: 50px;
+  }  
+`
+const BigPoop = styled.span`
+  font-size: 50px;
+`
 class Page extends Component {
     render () {
         return (
             <div>
                 <Meta />
                 <Header />
+                <MyButton huge >Click me! <span className="fire">fire</span></MyButton>
+                <MyButton>Click me! <BigPoop>:fire:</BigPoop></MyButton>
                 { this.props.children }
             </div>
         )
